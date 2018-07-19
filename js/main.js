2d4d93b62ce87c89a933744043ccaecfa3a16d39
Maher-Abedah <m.abedah1@gmail.com>
2018-07-19 14:47:34 +0300

    bug: adding aria-label attribute to 'View Details' button.

--- js/main.js
+++ js/main.js
@@ -179,6 +179,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.setAttribute('aria-label',`more details about ${restaurant.name} Restaurant`)
   li.append(more)
 
   return li
